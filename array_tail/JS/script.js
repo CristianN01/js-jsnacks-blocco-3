ae76e183f60bdaae8fa8c50a1c6f6b3c7519824b
CristianN01 <cristiannechifor71@gmail.com>
2024-05-15 15:16:39 +0200

    correzzione esercizio array_tail

--- array_tail/JS/script.js
+++ array_tail/JS/script.js
@@ -1,18 +1,57 @@
 
 
-const clientNumber = Number.parseInt(prompt('quanti elementi desideri?'), 10)
-const list =[]
+const clientNumber = Number.parseInt(prompt('quanti elementi desideri?'), 10);
+let numbersArray = [];
 
-
-for (let index= 0; index < clientNumber; index++){
-    const randomNumber = Math.floor(Math.random()* 100) +1
-    list.push(randomNumber)
-    
-    
-    
-    console.log(list)
-
-
+if(Number.isNaN(clientNumber) || clientNumber > 20 || clientNumber < 5){
+    clientNumber = 10;
 }
 
+for (let index = 0; index < clientNumber; index++) {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    numbersArray.push(randomNumber)
+    
+}
+console.log(numbersArray);
+console.log(numbersArray.slice(-5));
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const list =[]
+
+
+// for (let index= 0; index < clientNumber; index++){
+//     const randomNumber = Math.floor(Math.random()* 100) +1
+//     list.push(randomNumber)
+    
+    
+    
+//     console.log(list)
+
+
+// }
