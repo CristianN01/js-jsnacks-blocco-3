427058f531522a1c785d97210c951a11e61aa0db
CristianN01 <cristiannechifor71@gmail.com>
2024-05-15 13:02:53 +0200

    secondo snack con while

--- snack-while/script.js
+++ snack-while/script.js
@@ -17,14 +17,31 @@
 // Crea due array che hanno un numero di elementi diversi. 
 // Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
 
-const firstArray = [5, 6, 3, 1, 8, 4]
-const secondArray = [5, 6, 3, 1, 8, 4, 6, 9, 4, 5]
-console.log(firstArray, secondArray)
+// const firstArray = [5, 6, 3, 1, 8, 4]
+// const secondArray = [5, 6, 3, 1, 8, 4, 6, 9, 4, 5]
+// console.log(firstArray, secondArray)
 
-while( firstArray.length < secondArray.length){
-    let numbers = Math.floor(Math.random()* 100) +1
-    firstArray.push(numbers)
+// while( firstArray.length < secondArray.length){
+//     let numbers = Math.floor(Math.random()* 100) +1
+//     firstArray.push(numbers)
 
+// }
+
+// console.log(firstArray)
+
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
+// Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+const array =[];
+let sum = 0;
+
+while (sum < 50) {
+    let userNumber = Number.parseInt(prompt('Type a number'), 10);
+
+    if (Number.isNaN(userNumber) != true){
+        array.push(userNumber);
+        sum += userNumber;
+    }
 }
 
-console.log(firstArray)
+console.log(array, sum)
